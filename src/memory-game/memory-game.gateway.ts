ba9813a201fd172461fa4006175c110dbbe32990
Natan Lucena <zerogamerptbr@gmail.com>
2024-08-10 15:17:46 -0300

    :sparkles: feat: add aknowledgement

--- src/memory-game/memory-game.gateway.ts
+++ src/memory-game/memory-game.gateway.ts
@@ -16,14 +16,15 @@ import { Match } from 'src/match/match.schema';
 
 @WebSocketGateway()
 export class MemoryGameGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer() server: Server;
   private match: Match | null = null;
 
   constructor(
     @InjectModel('Team') private readonly teamModel: Model<Team>,
     @InjectModel('Match') private readonly matchModel: Model<Match>,
-  ) { }
+  ) {}
 
   afterInit(server: Server) {
     console.log('WebSocket server initialized');
@@ -42,6 +43,7 @@ export class MemoryGameGateway
     @MessageBody() data: { teamName: string },
     @ConnectedSocket() client: Socket,
   ): Promise<void> {
+    client.emit('acknowledgement', { event: 'joinGame', status: 'received' });
     if (!this.match) {
       this.match = new this.matchModel();
       await this.match.save();
@@ -61,6 +63,7 @@ export class MemoryGameGateway
 
   @SubscribeMessage('endGame')
   async handleEndGame(@ConnectedSocket() client: Socket): Promise<void> {
+    client.emit('acknowledgement', { event: 'endGame', status: 'received' });
     if (this.match) {
       this.server.to(this.match._id.toString()).emit('gameEnded');
       const teams = await this.teamModel.find({ matchId: this.match._id });
@@ -73,28 +76,29 @@ export class MemoryGameGateway
   }
 
   @SubscribeMessage('seeRanking')
-  async handleSeeRanking() {
+  async handleSeeRanking(@ConnectedSocket() client: Socket) {
+    client.emit('acknowledgement', { event: 'seeRanking', status: 'received' });
     const allTeams = await this.teamModel.find(
       {
-        score: { $gt: 0 }
+        score: { $gt: 0 },
       },
       null,
       {
         sort: {
-          score: -1
-        }
-      }
+          score: -1,
+        },
+      },
     );
 
     const ranking = allTeams.map((team) => {
       return {
         teamName: team.name,
-        score: team.score
-      }
+        score: team.score,
+      };
     });
 
     this.server.emit('showRanking', ranking);
 
-    console.log(ranking)
+    console.log(ranking);
   }
 }
